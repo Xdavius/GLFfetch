34ee3c6e7ccc909aa90ce6d3c10868a35dfd5dcb
Davius <66792063+Xdavius@users.noreply.github.com>
2024-05-05 14:50:11 +0200

    Update icon.sh

--- scripts/icon.sh
+++ scripts/icon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 file="/opt/GLFfetch/challenge.jsonc"
 os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2 | tr -d \")
-old_icon=$(grep key "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
-old_color=$(grep keyColor "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
+old_icon=$(grep key /opt/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
+old_color=$(grep keyColor /opt/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 
 case "$os" in
   "arch")
